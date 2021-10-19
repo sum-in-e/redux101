dd62c1a586796534682b2e5fd0382bc37b22ad56
sumin <suminkim.me@gmail.com>
2021-10-19 23:53:15 +0900

    Fix: 바닐라 자바스크립트 코드를 리덕스를 사용하여 변경

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-const handleUpdateCount = () => {
-  number.innerText = count;
+/**
+ * @remarks
+ * reducer는 함수이며, 데이터를 수정한다.
+ *
+ * @returns 수정한 데이터
+ */
+const countReducer = (count = 0, action) => {
+  // action을 전달 받아 어떤 수정을 할지 판단한다.
+  if (action.type === "PLUS") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
+/**
+ * @remarks
+ * createStore로 store를 생성한다.
+ */
+const countStore = createStore(countReducer);
+
+const handleChange = () => {
+  number.innerText = countStore.getState();
+};
+
+/**
+ * @remarks
+ * subscribe로 store의 변화를 구독한다. 변화가 일어날 때마다 인자로 전달한 함수가 실행 된다.
+ */
+countStore.subscribe(handleChange);
+
+/**
+ * @remarks
+ * 변화를 일으키기 위해 dispatch로 reducer에 action을 전달한다.
+ */
 const handlePlus = () => {
-  count += 1;
-  handleUpdateCount();
+  countStore.dispatch({ type: "PLUS" });
 };
+
 const handleMinus = () => {
-  count -= 1;
-  handleUpdateCount();
+  countStore.dispatch({ type: "MINUS" });
 };
 
 plus.addEventListener("click", handlePlus);
